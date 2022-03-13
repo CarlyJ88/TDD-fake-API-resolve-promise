993c7c0ad3b0ddbd6237a7c633204a1ad8e0da07
Carly <carly.jenkinson65@googlemail.com>
2022-03-13 22:52:41 +0000

    Extract data.

--- src/fakeAPI.js
+++ src/fakeAPI.js
@@ -1,16 +1,16 @@
+const people = [
+  {
+    name: "Rudolph",
+    hasPets: false,
+    currentTemp: 98.6,
+  },
+  {
+    name: "Henry",
+    hasPets: true,
+    currentTemp: 68.6,
+  },
+];
+
 export default function fakeAPICall(i) {
-  return Promise.resolve(
-    [
-      {
-        name: "Rudolph",
-        hasPets: false,
-        currentTemp: 98.6,
-      },
-      {
-        name: "Henry",
-        hasPets: true,
-        currentTemp: 68.6,
-      },
-    ][i]
-  );
+  return Promise.resolve(people[i]);
 }
